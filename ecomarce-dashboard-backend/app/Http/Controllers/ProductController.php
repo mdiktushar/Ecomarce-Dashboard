c06b3551d85f46b85a784b199c8ed8f40c191759
mdiktushar <md.tushar1804@gmail.com>
2021-12-21 21:33:47 +0600

    product api

---
+++ ecomarce-dashboard-backend/app/Http/Controllers/ProductController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\product;
+
+class ProductController extends Controller
+{
+    //
+
+    public function addProduct(Request $req)
+    {
+        # code...
+        $req->file('img')->store('products');
+        $product = new product;
+        $product->name = $req->name;
+        $product->price = $req->price;
+        $product->description = $req->description;
+        $product->file_path = $req->file('img')->store('products');
+        $product->save();
+        return $product;
+    }
+}
--- ecomarce-dashboard-backend/routes/api.php
+++ ecomarce-dashboard-backend/routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProductController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -20,4 +21,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::post('/register', [UserController::class,'register']); 
-Route::post('/login', [UserController::class,'login']); 
+Route::post('/login', [UserController::class,'login']);
+Route::post('/addproducts', [ProductController::class,'addProduct']);
